6310b7378aeb736c7cd59f5d8939debbf4390403
biniam-bin <biniamdnl@gmail.com>
2022-03-17 19:53:01 +0300

    make it better

---
+++ app.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+
+while True:
+    op = webdriver.ChromeOptions()
+    op.add_argument("--headless")
+    op.add_argument("--no-sandbox")
+    op.add_argument("--disable-dev-sh-usage")
+
+
+    driver = webdriver.Chrome(options=op)
+    driver.get("https://bit.ly/3CPQ4B7")
+
+
+# print(driver.page_source)
