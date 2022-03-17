586664afcc5db06146be9e0bdd8fcab3f940b5a9
biniam-bin <biniamdnl@gmail.com>
2022-03-17 21:22:30 +0300

    make it better001234567899

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from selenium import webdriver
 import os
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 while True:
+    s=Service(ChromeDriverManager().install())
     op = webdriver.ChromeOptions()
     op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     op.add_argument("--headless")
     op.add_argument("--no-sandbox")
     op.add_argument("--disable-dev-sh-usage")
 
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=op)
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=op, service=s)
     driver.get("https://bit.ly/3CPQ4B7")
 
 
